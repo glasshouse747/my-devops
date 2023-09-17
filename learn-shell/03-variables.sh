9b519c8481800cf988e72573d794561cc48f93e6
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 13:54:28 +0100

    Expense-bash script commit

---
+++ learn-shell/03-variables.sh
@@ -0,0 +1,12 @@
+# giving data a name makes it a variable
+
+# syntax for declaring a variable
+# variable name is= data (var=data)
+a=10
+x=100
+y=true
+name=DevOpsGuy
+
+# How to access a variable
+$a
+echo value of a = $a
