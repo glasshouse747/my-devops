87e829eb7bbccf2e87bea47e808499c718bd2855
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 14:45:38 +0100

    Expense-bash script commit

--- learn-shell/04-functions.sh
+++ learn-shell/04-functions.sh
@@ -1,6 +1,10 @@
+# If we give a name to a command then it's known as a function
 # How to declare a function
 abc() {
   echo Hello World
+  echo Hello ABC
+  echo Hello 123
+
 }
 
 # How to access a function
