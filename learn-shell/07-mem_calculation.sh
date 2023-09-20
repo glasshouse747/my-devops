fb0dea40f6d68af27ecbe176d7c79da788a93b1c
Richard Okoro <glasshouse747@gmail.com>
2023-09-20 05:11:05 +0100

    Expense-bash script commit

--- learn-shell/07-mem_calculation.sh
+++ learn-shell/07-mem_calculation.sh
@@ -3,3 +3,15 @@ free=$(free | grep Mem | awk '{print $4}')
 
 used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l | awk -F . '{print $1}' )
 echo used_percentage
+
+# 0-50 -> good
+# 51 - 70 -> orange
+# >70 - red
+
+if [$used _percentage -lt 50]; then
+  echo Mem stat is GREEN
+elif [ $used_percentage -lt 70]; then
+  echo Mem stat is ORANGE
+else
+  echo Mem stat is RED
+fi
